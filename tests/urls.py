96f7fc0600332b936657a93a1c9f8dba5d51b7b8
campbell <campbell@dxclabs.com>
2020-09-29 07:54:11 +0100

    * update for Django 3 * remove deprecations from Django 3.0 and 3.1 * refactor requirements

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path
 
 
 urlpatterns = [
-    url(r'^', include('django_diagnostic.urls', namespace='django_diagnostic')),
+    path('', include('django_diagnostic.urls', namespace='django_diagnostic')),
 ]
