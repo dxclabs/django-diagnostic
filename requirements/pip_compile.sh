a99942a7872ebe8a2a2d5334a51c001df89efe8d
campbell <campbell@dxclabs.com>
2022-01-10 12:58:51 +0000

    * update requirements

--- requirements/pip_compile.sh
+++ requirements/pip_compile.sh
@@ -3,11 +3,10 @@ set -ex
 export CUSTOM_COMPILE_COMMAND="./scripts/pip_compile.sh"
 
 # for requirements
-# python -m piptools compile --generate-hashes requirements/requirements.in -o requirements/requirements.txt
-python3 -m piptools compile --upgrade requirements/requirements.in -o requirements/requirements.txt
-# python3.5 -m piptools compile --generate-hashes -P 'Django>=1.11,<2.0' "$@" -o requirements/requirements.txt
+python -m piptools compile --upgrade requirements/requirements.in -o requirements/requirements.txt
+# python3 -m piptools compile --upgrade requirements/requirements.in -o requirements/requirements.txt
 
 # for requirements-dev
-# python -m piptools compile --generate-hashes requirements/requirements-dev.in -o requirements/requirements-dev.txt
-python3 -m piptools compile --upgrade requirements/requirements-dev.in -o requirements/requirements-dev.txt
+python -m piptools compile --upgrade requirements/requirements-dev.in -o requirements/requirements-dev.txt
+# python3 -m piptools compile --upgrade requirements/requirements-dev.in -o requirements/requirements-dev.txt
 # python3.5 -m piptools compile --generate-hashes -P 'Django>=1.11,<2.0' "$@" -o requirements/requirements.txt
