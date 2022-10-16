58deeb2eb6e13eeffbd0def6db75d7457bcd1bb0
campbell <campbell@dxclabs.com>
2022-10-16 22:16:58 +0100

    update versions in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import os
 import re
 import sys
@@ -14,65 +13,65 @@ def get_version(*file_paths):
     """Retrieves the version from django_diagnostic/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+    raise RuntimeError("Unable to find version string.")
 
 
 # version = get_version("django_diagnostic", "__init__.py")
-version = '0.5.0'
+version = "1.0.0"
 
-if sys.argv[-1] == 'publish':
+if sys.argv[-1] == "publish":
     try:
         import wheel
+
         print("Wheel version: ", wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
     sys.exit()
 
-if sys.argv[-1] == 'tag':
+if sys.argv[-1] == "tag":
     print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system(f"git tag -a {version} -m 'version {version}'")
     os.system("git push --tags")
     sys.exit()
 
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
 
 setup(
-    name='django-diagnostic',
+    name="django-diagnostic",
     version=version,
     description="""provides diagnostic pages for django application""",
-    long_description=readme + '\n\n' + history,
-    author='Campbell McKilligan',
-    author_email='campbell@dxclabs.com',
-    url='https://github.com/dxclabs/django-diagnostic',
+    long_description=readme + "\n\n" + history,
+    author="Campbell McKilligan",
+    author_email="campbell@dxclabs.com",
+    url="https://github.com/dxclabs/django-diagnostic",
     packages=[
-        'django_diagnostic',
+        "django_diagnostic",
     ],
     include_package_data=True,
     install_requires=[
-        'gitpython>=2.1.11',
+        "gitpython>=2.1.11",
     ],
     license="MIT",
     zip_safe=False,
-    keywords='django-diagnostic',
+    keywords="django-diagnostic",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
